1bcbe59b23e2c30590813a80f515dc732a0d7770
OlvynChuru <matthewsokolovsky@aol.com>
2019-09-19 12:54:17 -0400

    Added a new spell

--- iwd2ee/ieex_override/IEex_IWD2.lua
+++ iwd2ee/ieex_override/IEex_IWD2.lua
@@ -183,7 +183,6 @@ end
 --  rather than a number.
 -- This function can be used to find where a certain stat is stored in the creature's data.
 function EXSEARCH(effectData, creatureData)
-	local targetID = IEex_ReadDword(creatureData + 0x34)
 	local is_string = IEex_ReadDword(effectData + 0x40)
 	local search_end = IEex_ReadDword(effectData + 0x44)
 	if is_string > 0 then
@@ -206,6 +205,93 @@ function EXSEARCH(effectData, creatureData)
 				IEex_DisplayString("Match found for " .. search_byte .. " at offset " .. i .. " (1 byte)")
 			end
 		end
+	end
+end
+
+classstatnames = {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Wizard"}
+function MESTATPR(effectData, creatureData)
+	local targetID = IEex_GetActorIDShare(creatureData)
+	IEex_DisplayString(IEex_GetActorName(targetID))
+	local levelsum = IEex_GetActorStat(targetID, 95)
+	local classlevels = {}
+	for i = 96, 106, 1 do
+		local leveli = IEex_GetActorStat(targetID, i)
+		if leveli > 0 then
+			table.insert(classlevels, {classstatnames[i - 95], leveli})
+		end
+	end
+	local levelstring = "Level " .. levelsum
+	local numclasses = #classlevels
+	if numclasses == 1 then
+		levelstring = levelstring .. " " .. classlevels[1][1]
+	elseif numclasses > 1 then
+		levelstring = levelstring .. " ("
+		for i = 1, numclasses, 1 do
+			levelstring = levelstring .. classlevels[i][1] .. " " .. classlevels[i][2]
+			if i < numclasses then
+				levelstring = levelstring .. ", "
+			end
+		end
+		levelstring = levelstring .. ")"
+	end
+	IEex_DisplayString(levelstring)
+	
+	IEex_DisplayString("Hit Points: " .. IEex_ReadSignedWord(creatureData + 0x5C0, 0x0) .. "/" .. IEex_GetActorStat(targetID, 1))
+--[[
+	local ac = IEex_GetActorStat(targetID, 2)
+	local acslashing = IEex_GetActorStat(targetID, 6)
+	local acpiercing = IEex_GetActorStat(targetID, 5)
+	local acbludgeoning = IEex_GetActorStat(targetID, 3)
+	local acmissile = IEex_GetActorStat(targetID, 4)
+	if acslashing == acpiercing and acslashing == acbludgeoning and acslashing == acmissile then
+		IEex_DisplayString("Armor Class: " .. (ac + acslashing))
+	else
+		IEex_DisplayString("AC vs. slashing: " .. ac + acslashing .. "  AC vs. piercing: " .. ac + acpiercing .. "  AC vs. bludgeoning: " .. ac + acbludgeoning .. "  AC vs. missiles: " .. ac + acmissile)
+	end
+	if IEex_GetActorStat(targetID, 7) >= 0 then
+		IEex_DisplayString("Attack Bonus: +" .. IEex_GetActorStat(targetID, 7))
+	else
+		IEex_DisplayString("Attack Bonus: " .. IEex_GetActorStat(targetID, 7))
+	end
+--]]
+	IEex_DisplayString("Attacks per round: " .. IEex_GetActorStat(targetID, 8))
+	IEex_DisplayString("Ability Scores: ")
+	IEex_DisplayString("Strength: " .. IEex_GetActorStat(targetID, 36) .. "  Dexterity: " .. IEex_GetActorStat(targetID, 40) .. "  Constitution: " .. IEex_GetActorStat(targetID, 41) .. "  Intelligence: " .. IEex_GetActorStat(targetID, 38) .. "  Wisdom: " .. IEex_GetActorStat(targetID, 39) .. "  Charisma: " .. IEex_GetActorStat(targetID, 42))
+	IEex_DisplayString(MEGetStat(targetID, "Slashing Resistance: ", 21, "%\n") .. MEGetStat(targetID, "Piercing Resistance: ", 23, "%\n") .. MEGetStat(targetID, "Bludgeoning Resistance: ", 22, "%\n") .. MEGetStat(targetID, "Missile Resistance: ", 24, "%\n") .. MEGetStat(targetID, "Fire Resistance: ", 14, "%\n") .. MEGetStat(targetID, "Magical Fire Resistance: ", 19, "%\n") .. MEGetStat(targetID, "Cold Resistance: ", 15, "%\n") .. MEGetStat(targetID, "Magical Cold Resistance: ", 20, "%\n") .. MEGetStat(targetID, "Electricity Resistance: ", 16, "%\n") .. MEGetStat(targetID, "Acid Resistance: ", 17, "%\n") .. MEGetStat(targetID, "Poison Resistance: ", 74, "%\n") .. MEGetStat(targetID, "Magic Damage Resistance: ", 73, "%\n") .. MEGetStat(targetID, "Spell Resistance: ", 18, "%\n"))
+	IEex_DisplayString("Saving Throws: ")
+	IEex_DisplayString("Fortitude: " .. IEex_GetActorStat(targetID, 9) .. "  Reflex: " .. IEex_GetActorStat(targetID, 10) .. "  Will: " .. IEex_GetActorStat(targetID, 11))
+	IEex_DisplayString(MEGetStat(targetID, "Stoneskins remaining: ", 88, "\n") .. MEGetStat(targetID, "Casting time reduced by ", 77, "\n"))
+	if IEex_GetActorStat(targetID, 79) > 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " casts arcane spells as if " .. IEex_GetActorStat(targetID, 79) .. " levels higher")
+	end	
+	if IEex_GetActorStat(targetID, 53) ~= 100 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " casts arcane spells with " .. IEex_GetActorStat(targetID, 53) .. "% the normal duration")
+	end	
+	if IEex_GetActorStat(targetID, 80) > 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " casts divine spells as if " .. IEex_GetActorStat(targetID, 80) .. " levels higher")
+	end	
+	if IEex_GetActorStat(targetID, 54) ~= 100 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " casts divine spells with " .. IEex_GetActorStat(targetID, 54) .. "% the normal duration")
+	end	
+	if IEex_GetActorStat(targetID, 76) ~= 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " can cast more than one spell per round")
+	end
+	if IEex_GetActorStat(targetID, 81) ~= 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " can see invisible creatures")
+	end
+	if bit32.band(IEex_ReadByte(creatureData + 0x89F, 0), 0x2) ~= 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " is protected from critical hits")
+	end
+	if IEex_GetActorStat(targetID, 83) ~= 0 then
+		IEex_DisplayString(IEex_GetActorName(targetID) .. " cannot be reduced below " .. IEex_GetActorStat(targetID, 83) .. " HP")
+	end
+end
+
+function MEGetStat(targetID, pre, stat, post)
+	if IEex_GetActorStat(targetID, stat) == 0 then
+		return ""
+	else
+		return pre .. IEex_GetActorStat(targetID, stat) .. post
 	end
 end
 
