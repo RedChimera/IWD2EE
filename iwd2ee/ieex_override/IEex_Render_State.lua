544a73dcaf177fe3c907e625e3cf53e2c6155d9f
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2023-11-05 08:58:39 -0800

    Fix crash under Wine

--- iwd2ee/ieex_override/IEex_Render_State.lua
+++ iwd2ee/ieex_override/IEex_Render_State.lua
@@ -1,6 +1,10 @@
 
+-- This patches the WineD3D dll included in the GOG distribution's ddrawfix to enable logging.
+-- Enabling this on another version of WineD3D crashes the game.
+IEex_PatchWineD3DLogging = false
+
 function IEex_Extern_AfterDirectDrawCreate()
-	if IEex_Helper_FindLoadedModule("wined3d.dll") ~= 0x0 then
+	if IEex_PatchWineD3DLogging and IEex_Helper_FindLoadedModule("wined3d.dll") ~= 0x0 then
 		IEex_RunWithStack(0x4, function(esp)
 			IEex_DllCall("Kernel32", "VirtualProtect", {esp, 0x40, 0xAE00, 0x6FEC1000}, nil, 0x0)
 			IEex_HookReturnNOPs(0x6FEC1F6F, 0, {[[
