c7e9435aa9372968e8161122cbda18e7b817b503
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2023-11-06 20:16:35 -0800

    Fix insane bug in sound system
    - IWD2 relied on undefined behavior during boot!

--- iwd2ee/ieex_override/IEex_Sound_Patch.lua
+++ iwd2ee/ieex_override/IEex_Sound_Patch.lua
@@ -14,6 +14,17 @@
 		!pop_all_registers_iwd2
 	]]})
 
+	------------------------------------------------------------------------------------------------------------
+	-- Fix crash involving sound system not properly initializing CSoundMixer's pDirectSound ([+0x4]) member. --
+	-- pDirectSound is expected to be initialized to nullptr, but the engine never assigns it a value in      --
+	-- CSoundMixer's constructor. Thus, it starts as whatever value existed in its memory location. The       --
+	-- engine crashes if this memory location happens to hold a non-zero value during launch!                 --
+	------------------------------------------------------------------------------------------------------------
+
+	IEex_HookRestore(0x7AAD80, 0, 6, {[[
+		!mov([ecx+0x4],0x0)
+	]]})
+
 	IEex_EnableCodeProtection()
 
 end)()
